0ee4b6182e2435e6b7b596f2c5abe1bf2a56a22d
ash16888 <ash168@yandex.ru>
2021-08-25 11:01:09 +0300

    final

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cbr2Sql
 Python script allow you to download official exchange rates from www.cbr.ru in Sql
+Just input currency from list https://cbr.ru/eng/currency_base/daily/
+And you will get a database  file with  the entire history of observations.
+
+
--- run.py
+++ run.py
@@ -1,35 +1,15 @@
+"""#Script get exchange rates from cbr.ru to sqlite """
 import pandas as pd
 from sqlalchemy import create_engine
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+import datetime
 
-def find_key(input_dict, value):
-    for key in input_dict.keys():
-        if value in input_dict[key]:
-            return key
-    for key in input_dict.keys():
-        if not value in input_dict[key]:
-            return value
 
-def get_html(url):
-    data1 = pd.read_html(url, skiprows=2)[0]
-    df = pd.DataFrame(data1)
-    df.columns=['Date', 'Unit', 'Rate']
-    df.loc[:,'Date'] = pd.to_datetime(df['Date'])
-
-    engine = create_engine('sqlite:///current.db', echo=True)
-    sqlite_connection = engine.connect()
-
-    sqlite_table = 'table'
-    df.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
-    sqlite_connection.close()
-
-def main():
-    url_dict = 'https://cbr.ru/eng/currency_base/dynamics/'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
-    response = requests.get(url=url_dict, headers=headers)
+def curr_dict(url_list):
+    """Make dictionary with list of currency and codes"""
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
+    response = requests.get(url=url_list, headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')
     codes = soup.find_all('option')
     data = dict()
@@ -37,21 +17,46 @@ def main():
         name = code['value']
         value = code.text.strip()
         data[name] = value
-    val = input("Enter currency, like Euro, US Dollar full list on https://cbr.ru/eng/currency_base/daily/: ")
-    currency = find_key(data, val)
-    today = datetime.today().strftime('%d/%m/%Y')
-    url = 'https://cbr.ru/eng/currency_base/dynamics/?UniDbQuery.Posted=True&UniDbQuery.so=1&UniDbQuery.mode=1&UniDbQuery.date_req1=&UniDbQuery.date_req2=&UniDbQuery.VAL_NM_RQ=' + currency + '&UniDbQuery.From=01/07/1992&UniDbQuery.To=' + today
-    get_html(url)
+    return data
+
+
+def find_key(input_dict, value):
+    """In dictionary find key attr to currency, if not quit"""
+    for key in input_dict.keys():
+        if value in input_dict[key]:
+            return key
+    for key in input_dict.keys():
+        if not value in input_dict[key]:
+            print("Please, try again  insert correct name")
+            quit()
+
+
+def get_sqlite(url, value, end_date):
+    """ Get HTML Table to Pandas DataFrame than convert ti sqlite"""
+    tab = pd.read_html(url, skiprows=2)[0]
+    df = pd.DataFrame(tab)
+    df.columns = ['Date', 'Unit', 'Rate']
+    value = value.replace(' ', '').lower()
+    end_date = end_date.replace('/', '')
+    engine = create_engine('sqlite:///' + value + end_date + '.db', echo=True)
+    sqlite_connection = engine.connect()
+    sqlite_table = 'table'
+    df.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
+    sqlite_connection.close()
+
+
+def main():
+    """Main"""
+    d = curr_dict('https://cbr.ru/eng/currency_base/dynamics/')
+    value = input("Enter currency, like Euro, US Dollar full list on https://cbr.ru/eng/currency_base/daily/: ")
+    curr_code = find_key(d, value)
+    """Rates for tomorrow"""
+    end_date = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%d/%m/%Y')
+    url = 'https://cbr.ru/eng/currency_base/dynamics/?UniDbQuery.Posted=True&UniDbQuery.so=1&UniDbQuery.mode=1&UniDbQuery.date_req1=&UniDbQuery.date_req2=&UniDbQuery.VAL_NM_RQ=' + curr_code + '&UniDbQuery.From=01/07/1992&UniDbQuery.To=' + end_date
+    get_sqlite(url, value, end_date)
+
 
 main()
-
-
-
-
-
-
-
-
 
 
 
